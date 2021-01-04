8f49e5e0f1fa1705f60ccc071684251ca5e66b0b
albert <528613910@qq.com>
2021-01-04 15:43:06 +0800

    新增打开vscode bat

--- 04-匹配开头结尾.py
+++ 04-匹配开头结尾.py
@@ -3,7 +3,8 @@
 import re
 
 # 匹配163.com的邮箱地址
-email_list = ["xiaoWang@163.com", "xiaoWang@163.comheihei", ".com.xiaowang@qq.com"]
+email_list = ["xiaoWang@163.com",
+              "xiaoWang@163.comheihei", ".com.xiaowang@qq.com"]
 
 for email in email_list:
     ret = re.match("[\w]{4,20}@163\.com", email)
@@ -11,6 +12,8 @@ for email in email_list:
         print("%s 是符合规定的邮件地址,匹配后的结果是:%s" % (email, ret.group()))
     else:
         print("%s 不符合要求" % email)
+
+print('*'*50)
 
 for email in email_list:
     # $匹配结尾
